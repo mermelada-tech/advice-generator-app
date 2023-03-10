adce52994c4542e4a1f89dd535f4385a75fa6830
nadiauj <nadiauj@ext.inditex.com>
2023-03-10 19:33:29 +0100

    Add structured file

---
+++ src/components/nujovich/AdviceGenerator.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { getAdvice } from '../../pages/entry/nujovich/util/getAdvice'
+import '../styles/main.css'
+
+export const AdviceGenerator = ({ advice }) => {
+  const [response, setResponse] = useState(advice)
+
+  const onClickButton = async () => {
+    const response = await getAdvice()
+    setResponse(response)
+  }
+
+  return (
+    <div className='nujovich-advice-container'>
+      <label className='nujovich-label'>{response}</label>
+      <button className='nujovich-buton' onClick={() => onClickButton()}>
+        Get advice
+      </button>
+    </div>
+  )
+}
